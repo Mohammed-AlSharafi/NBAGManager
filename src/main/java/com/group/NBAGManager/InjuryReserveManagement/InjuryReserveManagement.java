ae46c18061dcaf27727afad0f958d06c3aa5c1d4
Muhammad Mairaj <rayyanmairaj2@gmail.com>
2024-06-08 19:03:47 +0800

    Completed InjuryReserveManagement, mainly integrated db for keeping track of players

--- src/main/java/com/group/NBAGManager/InjuryReserveManagement/InjuryReserveManagement.java
+++ src/main/java/com/group/NBAGManager/InjuryReserveManagement/InjuryReserveManagement.java
@@ -1,116 +1,80 @@
 package com.group.NBAGManager.InjuryReserveManagement;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Stack;
+import com.group.NBAGManager.model.Player;
+import com.group.NBAGManager.model.RepositoryHandler;
+import com.group.NBAGManager.repository.TeamRepository;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import com.group.NBAGManager.model.Player;
-import com.group.NBAGManager.repository.TeamRepository;
+import java.awt.event.*;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Stack;
 
 public class InjuryReserveManagement {
 
     private JPanel panelMain;
-    private JLabel label1;
+    private JLabel heading;
     private JTable injuryTable;
     private JButton removeButton;
     private JButton backButton;
     private JButton addButton;
+    private JScrollPane scroll;
+    private JFrame frame;
 
-    private Stack<String> injuryStack;
+    private TeamRepository teamRepository;
+    private List<Player> availablePlayers;
+    private Stack<Player> injuredPlayers;
 
-    //similar to contract, just change queue to stack
+    // similar to contract, just change queue to stack
     public InjuryReserveManagement() {
-        injuryStack = new Stack<>();
+        teamRepository = RepositoryHandler.getInstance().getTeamRepository();
 
-        panelMain = new JPanel();
-        panelMain.setLayout(new BoxLayout(panelMain, BoxLayout.Y_AXIS));
+        // retrieve all available players
+        availablePlayers = teamRepository.findIsInjured(false);
 
-        label1 = new JLabel("Injury Reserve");
-        label1.setFont(new Font("MV Boli", Font.PLAIN, 20));
-        label1.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panelMain.add(label1);
+        // retrieve all injured players and stack them
+        List<Player> injured = teamRepository.findIsInjured(true);
+        injured.sort(((o1, o2) -> (o1.getInjuryDateTime().compareTo(o2.getInjuryDateTime()))));
+        injuredPlayers = new Stack<>();
+        injured.forEach(player -> injuredPlayers.push(player));
 
-        panelMain.add(Box.createVerticalStrut(20));
-
-        String[] columnNames = {"Player", "Status"};
-        DefaultTableModel tableModel = new DefaultTableModel(columnNames, 0);
-        injuryTable = new JTable(tableModel);
-
+        // update injured players table
         loadStackState();
 
-        JScrollPane tableScrollPane = new JScrollPane(injuryTable);
-        tableScrollPane.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panelMain.add(tableScrollPane);
-
-        panelMain.add(Box.createVerticalStrut(20));
-
-        addButton = new JButton("Add Player");
-        removeButton = new JButton("Remove Player");
-        backButton = new JButton("Back");
-
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
-        buttonPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        buttonPanel.add(addButton);
-        buttonPanel.add(Box.createHorizontalStrut(10)); // Add space between buttons
-        buttonPanel.add(removeButton);
-        buttonPanel.add(Box.createHorizontalStrut(10)); // Add space between buttons
-        buttonPanel.add(backButton);
-
-        panelMain.add(buttonPanel);
-
+        // Add listeners
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 addPlayerFromRepository();
             }
         });
-
-        backButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                saveStackState();
-            }
-        });
-    }
-
-    public void displayForm() {
-        JFrame frame = new JFrame("Injury Reserve");
-        frame.setContentPane(panelMain);
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setSize(1250, 700);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.setVisible(true);
-
-        backButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                saveStackState();
-                frame.dispose();
-            }
-        });
-
         removeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 removePlayerFromStack();
             }
         });
-
-        frame.addWindowListener(new java.awt.event.WindowAdapter() {
+        backButton.addActionListener(new ActionListener() {
             @Override
-            public void windowClosing(java.awt.event.WindowEvent e) {
-                saveStackState();
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
+    }
+
+    public void displayForm() {
+        frame = new JFrame("Injury Reserve");
+        frame.setContentPane(panelMain);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(1000, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
                 super.windowClosing(e);
             }
         });
@@ -120,50 +84,74 @@ public class InjuryReserveManagement {
         JFrame playerListFrame = new JFrame("Select Player");
         playerListFrame.setSize(500, 400);
         playerListFrame.setLocationRelativeTo(null);
-
-        TeamRepository teamRepository = new TeamRepository();
-        List<Player> availablePlayers = new ArrayList<>(teamRepository.findAll());
+        playerListFrame.setVisible(true);
 
         Object[][] rowData = new Object[availablePlayers.size()][2];
         for (int i = 0; i < availablePlayers.size(); i++) {
-            rowData[i][0] = availablePlayers.get(i).getFirstName() + " " + availablePlayers.get(i).getLastName();
+            rowData[i][0] = availablePlayers.get(i).getFullName();
             rowData[i][1] = "Active";
         }
 
         String[] columnNames = {"Player", "Status"};
-        DefaultTableModel tableModel = new DefaultTableModel(rowData, columnNames);
-        JTable playerTable = new JTable(tableModel);
+        DefaultTableModel tableModel = new DefaultTableModel(rowData, columnNames) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
 
-        JScrollPane scrollPane = new JScrollPane(playerTable);
+            @Override
+            public Class<?> getColumnClass(int columnIndex) {
+                switch (columnIndex) {
+                    case 0:
+                        return Player.class;
+                    case 1:
+                        return String.class;
+                    default:
+                        return Object.class;
+                }
+            }
+        };
+
+        JTable addPlayerTable = new JTable(tableModel);
+        JScrollPane scrollPane = new JScrollPane(addPlayerTable);
         playerListFrame.add(scrollPane);
 
-        playerTable.addMouseListener(new MouseAdapter() {
+        addPlayerTable.addMouseListener(new MouseAdapter() {
+            private Player findPlayerByName(String name) {
+                for (Player player: availablePlayers) {
+                    if (player.getFullName().equals(name)) {
+                        return player;
+                    }
+                }
+                return null;
+            }
+
             @Override
             public void mouseClicked(MouseEvent e) {
-                int selectedRow = playerTable.getSelectedRow();
-                if (selectedRow != -1) {
-                    String playerName = (String) playerTable.getValueAt(selectedRow, 0);
+                if (e.getClickCount() == 2) {
+                    int selectedRow = addPlayerTable.getSelectedRow();
+                    if (selectedRow != -1) {
+                        String playerName = (String) addPlayerTable.getValueAt(selectedRow, 0);
+                        Player player = findPlayerByName(playerName);
+                        if (player == null) throw new RuntimeException("player should not be null at this point");
 
-                    DefaultTableModel injuryTableModel = (DefaultTableModel) injuryTable.getModel();
-                    injuryTableModel.addRow(new Object[]{playerName, "Injured"});
-                    injuryStack.push(playerName); //add to stack
+                        // mark player as injured and update database
+                        player.setInjured(true);
+                        player.setInjuryDateTime(LocalDateTime.now());
+                        teamRepository.update(player);
 
-                    for (Iterator<Player> iterator = availablePlayers.iterator(); iterator.hasNext();) {
-                        Player player = iterator.next();
-                        if ((player.getFirstName() + " " + player.getLastName()).equals(playerName)) {
-                            iterator.remove();
-                            break;
-                        }
+                        // remove the player from displayed available players
+                        tableModel.removeRow(availablePlayers.indexOf(player));
+
+                        // update lists of available and injured players
+                        availablePlayers.remove(player);
+                        injuredPlayers.push(player);
+
+                        // update the injured players table
+                        loadStackState();
                     }
-
-                    Object[][] updatedRowData = new Object[availablePlayers.size()][2];
-                    for (int i = 0; i < availablePlayers.size(); i++) {
-                        updatedRowData[i][0] = availablePlayers.get(i).getFirstName() + " " + availablePlayers.get(i).getLastName();
-                        updatedRowData[i][1] = "Active";
-                    }
-                    tableModel.setDataVector(updatedRowData, columnNames);
+                    playerListFrame.dispose();
                 }
-                playerListFrame.dispose();
             }
         });
 
@@ -171,53 +159,53 @@ public class InjuryReserveManagement {
     }
 
     private void removePlayerFromStack() {
-        DefaultTableModel injuryTableModel = (DefaultTableModel) injuryTable.getModel();
-        if (!injuryStack.isEmpty()) {
-            String removedPlayer = injuryStack.pop(); //remove from stack
-            for (int i = 0; i < injuryTableModel.getRowCount(); i++) {
-                String playerName = (String) injuryTableModel.getValueAt(i, 0);
-                if (playerName.equals(removedPlayer)) {
-                    injuryTableModel.removeRow(i);
-                    break;
-                }
-            }
-        } else {
-            //empty stack
+        if (!injuredPlayers.isEmpty()) {
+            // update the injured players table
+            DefaultTableModel injuryTableModel = (DefaultTableModel) injuryTable.getModel();
+            injuryTableModel.removeRow(0);
+
+            // remove player from injuredPlayers list
+            Player player = injuredPlayers.pop();
+            // add player to availablePlayers list
+            availablePlayers.add(player);
+
+            // update database
+            player.setInjured(false);
+            player.setInjuryDateTime(null);
+            teamRepository.update(player);
+        }
+        else {
             JOptionPane.showMessageDialog(panelMain, "No players in the injury reserve stack.", "Stack Empty", JOptionPane.WARNING_MESSAGE);
         }
     }
 
-    private void saveStackState() {
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("injuryStack.ser"))) {
-            out.writeObject(injuryStack);
-
-            DefaultTableModel model = (DefaultTableModel) injuryTable.getModel();
-            int rowCount = model.getRowCount();
-            ArrayList<String[]> tableData = new ArrayList<>();
-            for (int i = 0; i < rowCount; i++) {
-                String[] row = new String[2];
-                row[0] = (String) model.getValueAt(i, 0);
-                row[1] = (String) model.getValueAt(i, 1);
-                tableData.add(row);
-            }
-            out.writeObject(tableData);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @SuppressWarnings("unchecked")
     private void loadStackState() {
-        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("injuryStack.ser"))) {
-            injuryStack = (Stack<String>) in.readObject();
+        // load in reverse order so the last item pushed to stack is displayed on top
+        String[] columnNames = {"Player", "Status"};
+        Object[][] data = new Object[injuredPlayers.size()][2];
 
-            DefaultTableModel model = (DefaultTableModel) injuryTable.getModel();
-            ArrayList<String[]> tableData = (ArrayList<String[]>) in.readObject();
-            for (String[] row : tableData) {
-                model.addRow(row);
-            }
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
+        for (int i = 0; i < injuredPlayers.size(); i++) {
+            data[i][0] = injuredPlayers.get(injuredPlayers.size()-1-i).getFullName();
+            data[i][1] = "Injured";
         }
+
+        DefaultTableModel tableModel = new DefaultTableModel(data, columnNames) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+
+            @Override
+            public Class<?> getColumnClass(int columnIndex) {
+                switch (columnIndex) {
+                    case 0:
+                    case 1:
+                        return String.class;
+                    default:
+                        return Object.class;
+                }
+            }
+        };
+        injuryTable.setModel(tableModel);
     }
 }
