5e04b03a60f28f89aca894a614aae5729aa878ba
Muhammad Mairaj <rayyanmairaj2@gmail.com>
2024-06-09 23:14:43 +0800

    Integrated API - In LoginMenu.java, ensure all player data is up to date before launching app - In Player.java, added constructor - In pom.xml, added dependency

--- src/main/java/com/group/NBAGManager/model/Player.java
+++ src/main/java/com/group/NBAGManager/model/Player.java
@@ -65,6 +65,31 @@ public class Player implements Comparable<Player> {
         this.isContractRenewQueued = isContractRenewQueued;
     }
 
+    //used for APIHandler
+    public Player(int playerId, String firstName, String lastName, int age, double height, double weight, String position) {
+        this.playerId = playerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+
+        switch (position) {
+            case "F":
+                position = "Forward";
+                break;
+            case "G":
+                position = "Guard";
+                break;
+            case "C":
+                position = "Center";
+                break;
+            default:
+                position = "";
+        }
+        this.position = position;
+    }
+
     //instance variables
     private int playerId;
     private String firstName;
