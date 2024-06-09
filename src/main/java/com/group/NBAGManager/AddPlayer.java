8b6e026d59c02adf330b0f24d7545e4e0b081d37
Muhammad Mairaj <rayyanmairaj2@gmail.com>
2024-06-09 21:50:25 +0800

    Improved formatting

--- src/main/java/com/group/NBAGManager/AddPlayer.java
+++ src/main/java/com/group/NBAGManager/AddPlayer.java
@@ -1,18 +1,23 @@
 package com.group.NBAGManager;
 
-import com.group.NBAGManager.model.*;
+import com.group.NBAGManager.model.CurrentSession;
+import com.group.NBAGManager.model.Player;
+import com.group.NBAGManager.model.RepositoryHandler;
 import com.group.NBAGManager.repository.PlayerRepository;
 import com.group.NBAGManager.repository.TeamRepository;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
 import javax.swing.text.NumberFormatter;
 import java.awt.*;
 import java.awt.event.*;
 import java.text.NumberFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AddPlayer {
     private JFrame frame;
@@ -32,8 +37,10 @@ public class AddPlayer {
     public AddPlayer() {
         teamRepository = RepositoryHandler.getInstance().getTeamRepository();
         playerRepository = RepositoryHandler.getInstance().getPlayerRepository();
+
         teamPlayers = teamRepository.findAll();
         marketPlayers = playerRepository.findAll();
+
         playersTable.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
@@ -41,6 +48,7 @@ public class AddPlayer {
                 System.out.println(playersTable.getSelectedRow());
             }
         });
+
         setPlayersTable();
         addMouseFunction();
         searchField.addKeyListener(new KeyAdapter() {
@@ -64,7 +72,7 @@ public class AddPlayer {
         return frame;
     }
 
-    private void setPlayersTable(){
+    private void setPlayersTable() {
         //load the table
         ArrayList<Player> unOwnedPlayers = new ArrayList<>(marketPlayers);
         unOwnedPlayers.removeAll(teamPlayers);
@@ -75,9 +83,10 @@ public class AddPlayer {
         };
         Object[][] data = new Object[unOwnedPlayers.size()][columnNames.length];
         playerMap.clear();
+
         for (int i = 0; i < unOwnedPlayers.size(); i++) {
             Player player = unOwnedPlayers.get(i);
-            String playerName = player.getFirstName()+" "+player.getLastName();
+            String playerName = player.getFirstName() + " " + player.getLastName();
             data[i][0] = playerName;
             data[i][1] = player.getAge();
             data[i][2] = player.getHeight();
@@ -88,12 +97,12 @@ public class AddPlayer {
             data[i][7] = player.getAssists();
             data[i][8] = player.getSteals();
             data[i][9] = player.getBlocks();
-            playerMap.put(playerName,player);
+            playerMap.put(playerName, player);
         }
 
         DefaultTableModel model = new DefaultTableModel(data, columnNames) {
             @Override
-            public Class<?> getColumnClass(int column){
+            public Class<?> getColumnClass(int column) {
                 return switch (column) {
                     case 0, 4 -> String.class;
                     case 1 -> Integer.class;
@@ -101,11 +110,13 @@ public class AddPlayer {
                     default -> Object.class;
                 };
             }
+
             @Override
             public boolean isCellEditable(int row, int column) {
                 return false;
             }
         };
+
         playersTable.setModel(model);
 
         // Adjust column widths
@@ -122,36 +133,36 @@ public class AddPlayer {
 
         // Set custom renderer for all columns
         for (int i = 0; i < playersTable.getColumnCount(); i++) {
-        playersTable.getColumnModel().getColumn(i).setCellRenderer(new CustomCellRenderer());
-    }
-        scroll.setViewportView(playersTable);
-}
-
-static class CustomCellRenderer extends DefaultTableCellRenderer {
-    @Override
-    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-        JLabel label = new JLabel(" " + value.toString());
-        label.setFont(new Font("Serif", Font.PLAIN, getFont().getSize()));
-        label.setOpaque(true);
-        if (isSelected) {
-            label.setBackground(table.getSelectionBackground());
-            label.setForeground(table.getSelectionForeground());
-        } else {
-            label.setBackground(table.getBackground());
-            label.setForeground(table.getForeground());
+            playersTable.getColumnModel().getColumn(i).setCellRenderer(new CustomCellRenderer());
         }
-        return label;
+        scroll.setViewportView(playersTable);
     }
-}
 
-    private void addMouseFunction(){
+    static class CustomCellRenderer extends DefaultTableCellRenderer {
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+            JLabel label = new JLabel(" " + value.toString());
+            label.setFont(new Font("Serif", Font.PLAIN, getFont().getSize()));
+            label.setOpaque(true);
+            if (isSelected) {
+                label.setBackground(table.getSelectionBackground());
+                label.setForeground(table.getSelectionForeground());
+            } else {
+                label.setBackground(table.getBackground());
+                label.setForeground(table.getForeground());
+            }
+            return label;
+        }
+    }
+
+    private void addMouseFunction() {
         playersTable.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                if(e.getClickCount()==2){
+                if (e.getClickCount() == 2) {
                     int row = playersTable.rowAtPoint(e.getPoint());
                     int column = playersTable.columnAtPoint(e.getPoint());
-                    if(!(row<0)){
+                    if (!(row < 0)) {
                         Player player = getPlayerFromRow(row);
                         handleRowClick(player);
                         setPlayersTable();
@@ -167,38 +178,40 @@ static class CustomCellRenderer extends DefaultTableCellRenderer {
         addPlayer.displayForm();
     }
 
-    private void handleRowClick(Player player){
-        if(checkPlayer(player)){
+    private void handleRowClick(Player player) {
+        if (checkPlayer(player)) {
             JOptionPane.showMessageDialog(null, "Player already in team.");
             return;
         }
         String message = "Add Player?";
-        String[] options = {"Yes","No"};
-        int response = JOptionPane.showOptionDialog(null, message, "Player addition", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE,null,options,null);
-        if(response==0){
-            if(!checkSize()) JOptionPane.showMessageDialog(null, "Team size will exceed cap. \nPlayer addition cancelled.");
-            else{
+        String[] options = {"Yes", "No"};
+        int response = JOptionPane.showOptionDialog(null, message, "Player addition", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, null);
+        if (response == 0) {
+            if (!checkSize())
+                JOptionPane.showMessageDialog(null, "Team size will exceed cap. \nPlayer addition cancelled.");
+            else {
                 JFormattedTextField salaryField = setTextField();
                 JPanel panel = new JPanel(new GridLayout(0, 1));
                 panel.add(new JLabel("Please enter player's salary: "));
                 panel.add(salaryField);
                 int input = JOptionPane.showConfirmDialog(null, panel, "Player Salary", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
-                if(input == JOptionPane.OK_OPTION){
-                    double salary = ((Number)salaryField.getValue()).doubleValue();
+                if (input == JOptionPane.OK_OPTION) {
+                    double salary = ((Number) salaryField.getValue()).doubleValue();
                     String salary_check = "Salary is under minimum. Players with more than 20.0 points per game have minimum salary of 3000.";
-                    salary_check+="Players with less points have minimum salary of 1000.";
-                    salary_check+="Your current player has "+player.getPoints()+" points.";
-                    if(checkPlayerSalary(player,salary))JOptionPane.showMessageDialog(null,salary_check);
-                    else{
-                        if(!checkTeamSalary(salary)) JOptionPane.showMessageDialog(null,"This player's salary will exceed the team salary cap.");
-                        else{
+                    salary_check += "Players with less points have minimum salary of 1000.";
+                    salary_check += "Your current player has " + player.getPoints() + " points.";
+                    if (checkPlayerSalary(player, salary)) JOptionPane.showMessageDialog(null, salary_check);
+                    else {
+                        if (!checkTeamSalary(salary))
+                            JOptionPane.showMessageDialog(null, "This player's salary will exceed the team salary cap.");
+                        else {
                             player.setSalary(salary);
                             teamRepository.save(player);
 
                             teamPlayers.add(player); //to update current cached team players
                             marketPlayers.remove(player); //to update current cached market players
 
-                            JOptionPane.showMessageDialog(null,"Player has been added to team.");
+                            JOptionPane.showMessageDialog(null, "Player has been added to team.");
                             teamPlayers = teamRepository.findAll();
                         }
                     }
@@ -217,7 +230,7 @@ static class CustomCellRenderer extends DefaultTableCellRenderer {
         DefaultTableModel model = (DefaultTableModel) playersTable.getModel();
         for (int row = 0; row < model.getRowCount(); row++) {
             Player player = getPlayerFromRow(row);
-            if ((player.getFullName()).toLowerCase().contains(playerName.toLowerCase())){
+            if ((player.getFullName()).toLowerCase().contains(playerName.toLowerCase())) {
                 playersTable.setRowSelectionInterval(row, row);
                 return;
             }
@@ -225,7 +238,7 @@ static class CustomCellRenderer extends DefaultTableCellRenderer {
 //        JOptionPane.showMessageDialog(panelMain, "Player with name " + playerName + " not found.", "Info", JOptionPane.INFORMATION_MESSAGE);
     }
 
-    private JFormattedTextField setTextField(){
+    private JFormattedTextField setTextField() {
         NumberFormat numberFormat = NumberFormat.getInstance();
         NumberFormatter numberFormatter = new NumberFormatter(numberFormat);
         numberFormatter.setValueClass(Integer.class);
@@ -238,39 +251,38 @@ static class CustomCellRenderer extends DefaultTableCellRenderer {
         return input;
     }
 
-    private boolean checkPlayer(Player player){
+    private boolean checkPlayer(Player player) {
         Player check = teamRepository.findById(player.getPlayerId());
-        return check!=null;
+        return check != null;
     }
 
-    private boolean checkTeamSalary(double Salary){
+    private boolean checkTeamSalary(double Salary) {
         System.out.println(Salary);
         double totalsalary = 0;
-        for(Player player : teamPlayers){
+        for (Player player : teamPlayers) {
             totalsalary += player.getSalary();
         }
-        totalsalary+=Salary;
+        totalsalary += Salary;
         System.out.println(totalsalary);
         return totalsalary <= 20000.0;
     }
 
-    private boolean checkPlayerSalary(Player player, double Salary){
+    private boolean checkPlayerSalary(Player player, double Salary) {
         double points = player.getPoints();
-        if(points>20.0){
-            return Salary<3000.0;
-        }
-        else return Salary<1000.0;
+        if (points > 20.0) {
+            return Salary < 3000.0;
+        } else return Salary < 1000.0;
     }
 
-    private boolean checkSize(){
-        return teamPlayers.size()<15;
+    private boolean checkSize() {
+        return teamPlayers.size() < 15;
     }
 
-    public void displayForm(){
+    public void displayForm() {
         frame = new JFrame("AddPlayer");
         frame.setContentPane(panelMain);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setSize(1000,500);
+        frame.setSize(1000, 500);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setVisible(true);
